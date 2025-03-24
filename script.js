085788aadad16104ce70aedb52b0c18148af47b3
Jakub <cjakub19@gmail.com>
2025-03-24 17:59:43 +0100

    blueyed__

--- script.js
+++ script.js
@@ -13,3 +13,39 @@ document.addEventListener("DOMContentLoaded", function () {
         setTimeout(() => typeWriterEffect(el, text, 200), idx * 500); // Wolniejsze pisanie (200ms)
     });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const apps = document.querySelectorAll('.app');
+    const videoScreen = document.querySelector('.video-screen');
+    const videoFrame = document.getElementById('video-frame');
+    const closeBtn = document.querySelector('.close-btn');
+
+    apps.forEach(app => {
+        app.addEventListener('click', () => {
+            const videoUrl = app.getAttribute('data-video');
+            videoFrame.src = videoUrl;
+            videoScreen.classList.add('active');
+        });
+    });
+
+    closeBtn.addEventListener('click', () => {
+        videoScreen.classList.remove('active');
+        videoFrame.src = "";
+    });
+});
+
+
+function updateDateTime() {
+    const now = new Date();
+    const options = {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    };
+    const timeString = now.toLocaleTimeString('pl-PL', options);
+    const dateString = now.toLocaleDateString('pl-PL');
+    document.getElementById('datetime').textContent = `${dateString} | ${timeString}`;
+}
+
+setInterval(updateDateTime, 1000);
+updateDateTime();
